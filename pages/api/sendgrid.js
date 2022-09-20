0d4ddfea26a6b34fcdc2773bccfa60557d498b43
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2022-09-21 04:39:53 +0530

    Contact form created complete

---
+++ pages/api/sendgrid.js
@@ -0,0 +1,26 @@
+import mail from "@sendgrid/mail";
+
+
+export default function handler(req, res) {
+    mail.setApiKey(process.env.SENDGRID_API_KEY)
+    const body = JSON.parse(req.body)
+    const message = `
+    Name: ${body.first_name}+${body.middle_name}+${body.last_name}rn
+    Email: ${body.email_id}rn
+    Message: ${body.message}
+    `
+    const message_subject = body.subject
+    const message_email = body.email_id
+
+
+    mail.send({
+        to: 'ashish@ashishbhoi.com',
+        cc: message_email,
+        from: 'no-reply@ashishbhoi.com',
+        subject: message_subject,
+        text: message,
+        html: message.replace(/rn/g, '<br />')
+    }).then(() => {
+        res.status(200).json({status: 'Ok', message: 'Contact message sent'})
+    })
+}
